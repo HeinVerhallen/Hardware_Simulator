da7495f9777d2ad3f575596a6fa726b19ce6e3ab
Hein Verhallen <92261377+HeinVerhallen@users.noreply.github.com>
2023-10-25 14:48:02 +0200

    wrote code for voltage level setting

--- Firmware/Battery_emulator/lib/CellEmulator/CellEmulator.cpp
+++ Firmware/Battery_emulator/lib/CellEmulator/CellEmulator.cpp
@@ -1,29 +1,62 @@
 #include "CellEmulator.h"
+#include "twi_master.h"
 
-CellEmulator::CellEmulator()
-{
+CellEmulator::CellEmulator(uint8_t address, uint8_t number){
+    potAddress = address;
+    cellNumber = number;
+    tw_init(TW_FREQ_250K, 0);
+}
+
+CellEmulator::~CellEmulator(){
 
 }
 
-CellEmulator::~CellEmulator()
-{
-
-}
-
-double CellEmulator::getVoltage()
-{
+double CellEmulator::getVoltage(){
+    //read potmeter1 value
+    int value = readWiperValue(1);
+    voltage = V_REF*N_STEPS/(N_STEPS-value);
     return voltage;
 }
 
-void CellEmulator::setVoltage(double voltage)
-{
+void CellEmulator::setVoltage(double voltage){
+    uint8_t value = N_STEPS-(V_REF*N_STEPS/voltage);
+    writeWiperValue(1, value);
+    //write potmeter1 value
 }
 
-double CellEmulator::getCurrent()
-{
+double CellEmulator::getCurrent(){
+    //read potmeter0 value
     return current;
 }
 
-void CellEmulator::setCurrent(double current)
-{
+void CellEmulator::setCurrent(double current){
+    //write potmeter0 value
+}
+
+uint8_t CellEmulator::readWiperValue(uint8_t wiperSelect){
+    selectI2Cbus(cellNumber);
+    uint8_t data[2] = {(wiperSelect<<4)&(0b11<<2)};
+    //                          register addres     read mode + 2reserved
+
+    //                  address     write   data   size repeated start true
+    tw_master_transmit((potAddress & 0xFE),&data[0],1,1);
+    tw_master_receive((potAddress | 0x1),&data[0], 2);
+    //                  address     read    data    size
+
+    return data[1];
+}
+
+void CellEmulator::writeWiperValue(uint8_t wiperSelect, uint8_t value){
+    selectI2Cbus(cellNumber);
+    uint8_t size = 2;    
+    uint8_t data[size] = {(wiperSelect<<4),value};
+    tw_master_transmit((potAddress & 0xFE),&data[0],size,0);
+    //                  address     write   data    size repeated start false
+}
+
+void selectI2Cbus(int cellNumber){
+    if(cellNumber > 1){
+        return;
+    }
+    PORTD |= (cellNumber<<I2C_BUS_SELECT_1) | (!cellNumber<<I2C_BUS_SELECT_2);
 }
--- Firmware/Battery_emulator/lib/CellEmulator/CellEmulator.h
+++ Firmware/Battery_emulator/lib/CellEmulator/CellEmulator.h
@@ -9,10 +9,26 @@
 	#include "WProgram.h"
 #endif
 
+#define N_DATA_BYTES 2
+#define I2C_BUS_SELECT_1 PD2
+#define I2C_BUS_SELECT_2 PD3
+
+// Vout = 320/(256-X) where X=potentiometer wiper value(8bit) and Vout is the battery voltage (regulator parameters: Vref = 1.25V, Iadj = 50uA) 
+/*Vout = Vref*Nsteps/(Nsteps-Regvalue) 
+where:
+Vout    = battery voltage, 
+Vref    = linear regulators internal reference voltage, 
+Nsteps  = number of steps the digital potentiometer has and 
+x       = value of the potentiometer wiper
+*/
+#define V_REF 1.25
+#define N_STEPS 256
+#define CALCULATE_VOLTAGE(x) V_REF*N_STEPS/(N_STEPS-x)
+
 class CellEmulator
 {
 public:
-    CellEmulator();
+    CellEmulator(uint8_t address, uint8_t number);
     ~CellEmulator();
     
     double getVoltage();
@@ -24,6 +40,27 @@ public:
 private:
     double voltage = 2.5;
     double current = 0.1;
+    uint8_t cellNumber = 0;
+    uint8_t potAddress = 0b01010000;
+    uint8_t I2Cbus = 0;
+    uint8_t wiperValue;
+
+    void setAdress(uint8_t adress);
+
+    uint8_t readWiperValue(uint8_t wiperSelect);
+    void writeWiperValue(uint8_t wiperSelect,uint8_t value);
 };
 
 #endif
+
+//I2C read sequence: 
+//address byte;                             command byte;                                   repeated start; control byte;                               data byte;              data byte;
+//fixed address;variable address;read/write //memory address(4 bit);command;    reserved(2bit)              //fixed address;variable addresss;read      //empty data(7 bit);D8  //data
+//"0101"        A2 A1 A0         0(write)   //(1->4)                11(read)    XX                          //"0101"        A2 A1 A0          1(read)   //"0000000"         MSB //D7->D0 
+
+
+//I2C write sequence: 
+//address byte;                             command byte;                                           data byte;
+//fixed address;variable address;read/write //memory address(4 bit);command;    reserved(1bit); D8  //data
+//"0101"        A2 A1 A0         0(write)   //(1->4)                00(write)   X               MSB //D7->D0 
+//memory address write      D8              D7->D0
