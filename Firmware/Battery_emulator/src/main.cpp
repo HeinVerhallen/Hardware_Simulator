8b4d8f70e4956c2d193f392b664e34b4fd23e777
Hein Verhallen <92261377+HeinVerhallen@users.noreply.github.com>
2024-01-18 12:43:00 +0100

    Refactor main.cpp: Remove unused variables and add test function

--- Firmware/Battery_emulator/src/main.cpp
+++ Firmware/Battery_emulator/src/main.cpp
@@ -6,38 +6,36 @@
 
 // initialize the cell emulators with their addresses and numbers
   CellEmulator pot1(0b00101000, 1);
-  CellEmulator pot2(0b00101001, 2);
-  CellEmulator pot3(0b00101010, 8);
+  // CellEmulator pot2(0b00101001, 2);
+  // CellEmulator pot3(0b00101010, 8);
+
+  void testFunction(uint8_t val){
+  Serial.print("current should be: ");
+  Serial.println(val);
+  // Serial.println(pot1.setVoltage((double)voltage),BIN);
+  Serial.println(pot1.setCurrent(val));
+}
 
 int main(void){
+  init();
   Serial.begin(9600);
 
   while(1){
     int size = 300;
     char buffer[size];
-    int idx = 0;
+    static int idx = 0;
 
     //check if data is available on the serial port
-    while(Serial.available() > 0)
-      {
-        if (idx > size){ //if the buffer is full
-          break;
-        } else{ //if the buffer is not full
-          buffer[idx] = Serial.read(); //read the data from the serial port
-        }
+    while(Serial.available() > 0){
+      if (idx > size) //if the buffer is full
+        break;
+      else //if the buffer is not full
+        buffer[idx] = Serial.read(); //read the data from the serial port
 
-        if(buffer[idx] == 0x72){ //if the data is a 'r'
-          testFunction(buffer[idx-1]-48); //call the test function
-        }
-        idx++;
-      }
-    } 
+      if(buffer[idx] == 0x72) //if the data is a 'r'
+        testFunction(buffer[idx-1]-48); //call the test function
+      idx++;
+    }
+  } 
   return 0;
-}
-
-void testFunction(uint8_t voltage){
-  Serial.print("voltage should be: ");
-  Serial.println(voltage);
-  // Serial.println(pot1.setVoltage((double)voltage),BIN);
-  Serial.println(pot1.setCurrent(voltage));
 }
